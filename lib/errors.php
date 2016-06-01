9f60d125fb0100fba1d00a17954ea486ee02839c
Carlos Escribano <carlos@doofinder.com>
2016-06-01 16:42:38 +0200

    PHP5.3 compatibility. String interpolation. Reindent. Fixed Typos.
    Signed-off-by: Carlos Escribano <carlos@doofinder.com>

--- lib/errors.php
+++ lib/errors.php
@@ -1,50 +1,48 @@
 <?php
 
-class NotAllowed extends Exception{
-}
+class NotAllowed extends Exception {}
 
-class BadRequest extends Exception{
-}
+class BadRequest extends Exception {}
 
-class QuotaExhausted extends Exception{
-}
+class QuotaExhausted extends Exception {}
 
-class WrongResponse extends Exception{
-}
+class WrongResponse extends Exception {}
 
-class ThrottledResponse extends Exception{
-}
+class ThrottledResponse extends Exception {}
 
 function readError($response) {
-    $error = json_decode($response, true)['detail'];
-    if(!isset($error)){
-        $error = $response;
-    }
-    return $error;
+  $error = json_decode($response, true);
+  $error = $error['detail'];
+  if (!isset($error)) {
+    $error = $response;
+  }
+  return $error;
 }
 
-
 function handleErrors($statusCode, $response){
-    switch($statusCode)
-    {
+  switch($statusCode)
+  {
     case 403:
-        throw new NotAllowed("The user does not have permissions to perform this operation: ".readError($response));
+      throw new NotAllowed("The user does not have permissions to perform this operation: ".readError($response));
     case 401:
-        throw new NotAllowed("The user hasn't provided valid authorization: ".readError($response));
+      throw new NotAllowed("The user hasn't provided valid authorization: ".readError($response));
     case 404:
-        throw new BadRequest("Not Found: ".readError($response));
+      throw new BadRequest("Not Found: ".readError($response));
     case 409: // trying to post with an already used id
-        throw new BadRequest("Request conflict: ".readError($response));
+      throw new BadRequest("Request conflict: ".readError($response));
     case 429:
-        if(stripos($response, 'throttled')){
-            throw new ThrottledResponse(readError($response));
-        }
+      if (stripos($response, 'throttled')) {
+        throw new ThrottledResponse(readError($response));
+      } else {
         throw new QuotaExhausted("The query quota has been reached. No more queries can be requested right now");
-    }
-    if($statusCode >= 500){
-        throw new WrongResponse("Server error: ".readError($response));
-    }
-    if($statusCode >= 400){
-        throw new BadRequest("The client made a bad request: ".readError($response));
-    }
+      }
+  }
+
+  if ($statusCode >= 500) {
+    throw new WrongResponse("Server error: ".readError($response));
+  }
+
+  if ($statusCode >= 400) {
+    throw new BadRequest("The client made a bad request: ".readError($response));
+  }
 }
