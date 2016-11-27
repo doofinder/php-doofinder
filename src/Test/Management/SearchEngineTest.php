f6c9dace71c6718f21d579529211301b75321cd8
Joe Z <jzarate@gmail.com>
2016-11-27 14:02:57 -0500

    finishing search engine items tests. starting stats tests

--- src/Test/Management/SearchEngineTest.php
+++ src/Test/Management/SearchEngineTest.php
@@ -4,6 +4,7 @@ namespace Doofinder\Api\Test;
 
 use Doofinder\Api\Management\SearchEngine;
 use Doofinder\Api\Management\ScrollIterator;
+use Doofinder\Api\Management\AggregatesIterator;
 
 class SearchEngineTest extends \PHPUnit_Framework_TestCase
 {
@@ -78,9 +79,9 @@ class SearchEngineTest extends \PHPUnit_Framework_TestCase
             'testHashid/items/newType',
             null,
             json_encode($item)
-        )->shouldBeCalledTimes(1);
+        )->shouldBeCalledTimes(1)->willReturn(array('response'=>array('id'=>'id1')));
 
-        $this->searchEngine->addItem('newType', $item);
+        $this->assertEquals('id1', $this->searchEngine->addItem('newType', $item));
 
     }
 
@@ -106,14 +107,89 @@ class SearchEngineTest extends \PHPUnit_Framework_TestCase
         )->shouldBeCalledTimes(1)->willReturn(
             array(
                 'response'=>array(
-                    array('id'=>'i1'), array('id'=>'i2')
+                    array('id'=>'id1'), array('id'=>'id2')
                 )
             )
         );
 
-        $this->searchEngine->addItems('newType', $items);
+        $result = $this->searchEngine->addItems('newType', $items);
+        // returns ids inserted
+        $this->assertEquals(array('id1', 'id2'), $result);
     }
 
+    public function testUpdateItemApiCall()
+    {
+        $item = array("id"=>"id1", "title"=>"title1", "cats"=>array("cat1", "cat2"));
+        $this->client->managementApiCall(
+            'PUT',
+            'testHashid/items/newType/idx',
+            null,
+            json_encode($item)
+        )->shouldBeCalledTimes(1)->willReturn(
+            array('statusCode' => 200));
+
+        $this->assertTrue($this->searchEngine->updateItem('newType', 'idx', $item));
+    }
+
+    public function testUpdateItemsApiCall()
+    {
+        $items = array(
+            array('id'=>'id1', 't'=>'t1', 'cats'=>array('cat1', 'cat2')),
+            array('id'=>'id2', 't'=>'t2', 'cats'=>array('cat1', 'cat2'))
+        );
+        $this->client->managementApiCall(
+            'PUT', 'testHashid/items/newType', null, json_encode($items)
+        )->willReturn(array('statusCode'=>200));
+
+        $this->assertTrue($this->searchEngine->updateItems('newType', $items));
+    }
+
+    public function testDeleteItemApiCall()
+    {
+        $this->client->managementApiCall('DELETE', 'testHashid/items/newType/id1')
+                     -> willReturn(array('statusCode'=>204));
+        $this->assertTrue($this->searchEngine->deleteItem('newType', 'id1'));
+    }
+
+    /**
+     * @param \DateTime $from_date
+     * @param \DateTime $to_date
+     *
+     * @dataProvider providerTestStatsReturnsAggregatesIteratorWithDifferentDates
+     */
+    public function testStatsReturnsAggregatesIteratorWithDifferentDates($from_date, $to_date)
+    {
+        if(is_null($from_date)){
+            $aggregatesIterator = $this->searchEngine->stats();
+        } else {
+            $aggregatesIterator = $this->searchEngine->stats($from_date, $to_date);
+        }
+        // returns a aggregatesIterator
+        $this->assertInstanceOf(
+            '\Doofinder\Api\Management\AggregatesIterator', $aggregatesIterator
+        );
+        // with the right searchEngine
+        $this->assertEquals(
+            $this->searchEngine, $this->exposeAttribute($aggregatesIterator, 'searchEngine')
+        );
+        // and proper dates
+        if(is_null($from_date)){
+            $expected = array();
+        } else {
+            $expected = array('from'=>$from_date->format("Ymd"), 'to'=>$to_date->format("Ymd"));
+        }
+        $this->assertEquals(
+            $expected, $this->exposeAttribute($aggregatesIterator, 'searchParams')
+        );
+    }
+
+    public function providerTestStatsReturnsAggregatesIteratorWithDifferentDates()
+    {
+        return array(
+            array(null, null), //default
+            array(new \DateTime("20011-01-07"), new \DateTime("2011-02-07"))
+        );
+    }
 
 
 
