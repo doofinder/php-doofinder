9a7f012af6975bea52fdb2f80585d5fa34b93c15
Joe Z <jzarate@gmail.com>
2016-11-28 23:57:55 -0500

    make tests: TopTermsIterator

--- src/Test/Management/TopTermsIteratorTest.php
+++ src/Test/Management/TopTermsIteratorTest.php
@@ -14,12 +14,32 @@ class TopTermsIteratorTest extends \PHPUnit_Framework_TestCase
         $this->searchEngine = new SearchEngine($this->client->reveal(), 'testHashid', 'test SE');
     }
 
-    public function testIterateThroughAllTopTerms()
+    /**
+     * @expectedException \Doofinder\Api\Management\Errors\NotProcessedResponse
+     */
+    public function testNotProcessedResponse()
+    {
+        $this->client->managementApiCall('GET', "testHashid/stats/top_fakeTerm", [])
+                     ->shouldBeCalledTimes(1)
+                     ->willReturn(array('statusCode'=>202));
+
+        $terms = new TopTermsIterator($this->searchEngine, 'fakeTerm');
+        foreach($terms as $t){
+            echo "boo";
+        }
+    }
+
+    /**
+     * @param string $termName
+     *
+     * @dataProvider providerTermNames
+     */
+    public function testIterateThroughAllTopTerms($termName)
     {
         $firstResponse = array(
             "next"=> "someUrl", "count"=>3, "previous"=>null,
             "start"=> '2016-10-20', 'end'=> '2016-10-23',
-            "searches" => array(
+            $termName => array(
                 array("term"=> "t1", "count"=> 1),
                 array("term"=> "t2", "count"=> 1)
             )
@@ -27,15 +47,17 @@ class TopTermsIteratorTest extends \PHPUnit_Framework_TestCase
         $secondResponse = array(
             "next"=> "someUrl", "count"=>3, "previous"=>null,
             "start"=> '2016-10-20', 'end'=> '2016-10-23',
-            "searches" => array(
+            $termName => array(
                 array("term"=> "t3", "count"=> 1)
             )
         );
 
         // after receiving first request, prepare for second request and send first response
-        $prepareForSecondRequest = function($args, $client) use ($secondResponse, $firstResponse) {
+        $prepareForSecondRequest = function($args, $client) use (
+            $termName, $secondResponse, $firstResponse
+        ) {
             $client->managementApiCall(
-                'GET', 'testHashid/stats/top_searches',
+                'GET', "testHashid/stats/top_$termName",
                 array('from'=>'20161020', 'to'=>'20161023', 'page'=>2)
             )
                 ->shouldBeCalledTimes(1)
@@ -45,14 +67,14 @@ class TopTermsIteratorTest extends \PHPUnit_Framework_TestCase
 
         // first request
         $this->client->managementApiCall(
-            'GET', 'testHashid/stats/top_searches', array('from'=>'20161020', 'to'=>'20161023')
+            'GET', "testHashid/stats/top_$termName", array('from'=>'20161020', 'to'=>'20161023')
         )
                      ->shouldBeCalledTimes(1)
                      ->will($prepareForSecondRequest);
 
 
         $terms = new TopTermsIterator(
-            $this->searchEngine, 'searches', new \Datetime('2016-10-20'), new \Datetime('2016-10-23')
+            $this->searchEngine, $termName, new \Datetime('2016-10-20'), new \Datetime('2016-10-23')
         );
 
         $counter = 0;
@@ -60,6 +82,15 @@ class TopTermsIteratorTest extends \PHPUnit_Framework_TestCase
             $counter++;
             $this->assertEquals("t$counter", $term['term']);
         }
+    }
+
+    public function providerTermNames()
+    {
+        return array(
+            array('searches'),
+            array('clicked'),
+            array('opportunities')
+        );
     }
 
 }
