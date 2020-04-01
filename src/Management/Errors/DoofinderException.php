d716028ad610586cb33153369718cf72167f9f0c
Domingo Arteaga <domingoarte2850@gmail.com>
2020-04-01 12:57:12 +0200

    Remove Management V1 folder

--- src/Management/Errors/DoofinderException.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace Doofinder\Api\Management\Errors;
-
-class DoofinderException extends \Exception {}
--- src/Management/Errors/TypeAlreadyExists.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace Doofinder\Api\Management\Errors;
-
-class TypeAlreadyExists extends DoofinderException {}
