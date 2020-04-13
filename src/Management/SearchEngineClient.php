1bcb389f30ee7f4b9c87f882eabfdaa5e0335818
Domingo Arteaga <domingoarte2850@gmail.com>
2020-04-13 18:11:32 +0200

    Add new namespace

--- src/Management/SearchEngineClient.php
+++ src/Management/SearchEngineClient.php
@@ -2,10 +2,10 @@
 /**
  * Class with all the capabilities described in the API
  *
- * see http://www.doofinder.com/developer/topics/api/management-api
+ * see #
  */
 
-namespace DoofinderManagement;
+namespace Doofinder\Management;
 
 use DoofinderManagement\Api\SearchEnginesApi;
 use GuzzleHttp\Client;
@@ -15,22 +15,37 @@ class SearchEngineClient {
     public $config = null;
     public $api = null;
   
-  
+    /**
+     * Create a new SearchEngineClient instance
+     * 
+     * @param DoofinderManagement\Configuration $config instance previously created (required)
+     * @return serchEngineCliente instance created
+     */
     public function __construct($config) {
       $this->config = $config;
-      $this->api = new Api\SearchEnginesApi(
+      $this->api = new SearchEnginesApi(
         new Client(),
         $config
       );
     }
     /**
-     * Add SearchEngine
+     * Create SearchEngine
      * 
      * @param object $body attributes to create a Search Engine (required)
      * @return serchEngine object created
      */
-    public function addSearchEngine($body) {
+    public function createSearchEngine($body) {
       return $this->api->searchEngineCreate($body);
+    }
+    
+    /**
+     * Get SearchEngine
+     *
+     * @param string $hashid Unique id of a search engine. (required)
+     * @return serchEngine object
+     */
+    public function getSearchEngine($hashid) {
+      return $this->api->searchEngineShow($hashid);
     }
     
     /**
@@ -49,9 +64,18 @@ class SearchEngineClient {
      * @param  string $hashid Unique id of a search engine. (required)
      * @return searchEngine object, updated
      */
-    public function updateSearchEngine($body, $hashid){
+    public function updateSearchEngine($hashid, $body){
       return $this->api->searchEngineUpdate($body, $hashid);
     }
+
+    /**
+     * List SearchEngines
+     *
+     * @return searchEngine array
+     */
+    public function listSearchEngines(){
+        return $this->api->searchEngineList();
+      }
   
   }
   
