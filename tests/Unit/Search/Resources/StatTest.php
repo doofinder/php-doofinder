49c89d5255987cc0dccc27fcaa1ffce8c35b0db1
Gabriel <gabriel@doofinder.com>
2022-01-18 07:57:31 +0100

    BK-719 Create remove from cart request

--- tests/Unit/Search/Resources/StatTest.php
+++ tests/Unit/Search/Resources/StatTest.php
@@ -722,4 +722,122 @@ class StatTest extends BaseResourceTest
 
         $this->assertTrue($thrownException);
     }
+
+    public function testLogRemoveFromCartSuccess()
+    {
+        $body = ['result' => 'registered'];
+
+        $response = HttpResponse::create(HttpStatusCode::OK, json_encode($body));
+
+        $sessionId = 'rand_fake_session_id';
+        $id = 'fake_id';
+        $hashId = '3a0811e861d36f76cedca60723e03291';
+        $amount = 2;
+        $indexName = 'fake_index';
+
+        $this->httpClient
+            ->expects($this->once())
+            ->method('request')
+            ->with(
+                $this->getUrl($hashId) . '/cart/' . $sessionId,
+                HttpClientInterface::METHOD_PATCH,
+                [
+                    'amount' => $amount,
+                    'id' => $id,
+                    'index' => $indexName,
+                ],
+                ['Authorization: Token ' . self::TOKEN]
+            )
+            ->willReturn($response);
+
+        $this->setConfig();
+
+        $response = $this->createSut()->logRemoveFromCart($hashId, $sessionId, $amount, $id, $indexName);
+
+        $this->assertSame(HttpStatusCode::OK, $response->getStatusCode());
+        $this->assertEquals($response->getBody(), $body);
+    }
+
+    public function testLogRemoveFromCartInvalidParams()
+    {
+        $response = HttpResponse::create(HttpStatusCode::BAD_REQUEST, '{"error" : {"code": "bad_params"}}');
+        $hashId = '3a0811e861d36f76cedca60723e03291';
+        $sessionId = 'rand_fake_session_id';
+        $id = 'fake_id';
+        $amount = 2;
+        $indexName = 'fake_index';
+
+        $this->httpClient
+            ->expects($this->once())
+            ->method('request')
+            ->with(
+                $this->getUrl($hashId) . '/cart/' . $sessionId,
+                HttpClientInterface::METHOD_PATCH,
+                [
+                    'amount' => $amount,
+                    'id' => $id,
+                    'index' => $indexName,
+                ],
+                ['Authorization: Token ' . self::TOKEN]
+            )
+            ->willReturn($response);
+
+        $this->setConfig();
+
+        $thrownException = false;
+
+        try {
+            $this->createSut()->logRemoveFromCart($hashId, $sessionId, $amount, $id, $indexName);
+        } catch (ApiException $e) {
+            $thrownException = true;
+            $this->assertSame(HttpStatusCode::BAD_REQUEST, $e->getCode());
+            /** @var HttpResponseInterface $response */
+            $response = $e->getBody();
+            $this->assertSame('bad_params', $response->getBody()['error']['code']);
+        }
+
+        $this->assertTrue($thrownException);
+    }
+
+    public function testLogRemoveFromCartHashIdNotFound()
+    {
+        $hashId = '3a0811e861d36f76cedca60723e03291';
+
+        $response = HttpResponse::create(HttpStatusCode::NOT_FOUND, '{"error" : {"code": "not_found"}}');
+        $sessionId = 'rand_fake_session_id';
+        $id = 'fake_id';
+        $amount = 2;
+        $indexName = 'fake_index';
+
+        $this->httpClient
+            ->expects($this->once())
+            ->method('request')
+            ->with(
+                $this->getUrl($hashId) . '/cart/' . $sessionId,
+                HttpClientInterface::METHOD_PATCH,
+                [
+                    'amount' => $amount,
+                    'id' => $id,
+                    'index' => $indexName,
+                ],
+                ['Authorization: Token ' . self::TOKEN]
+            )
+            ->willReturn($response);
+
+        $this->setConfig();
+
+        $thrownException = false;
+
+        try {
+            $this->createSut()->logRemoveFromCart($hashId, $sessionId, $amount, $id, $indexName);
+        } catch (ApiException $e) {
+            $thrownException = true;
+            $this->assertSame(HttpStatusCode::NOT_FOUND, $e->getCode());
+            /** @var HttpResponseInterface $response */
+            $response = $e->getBody();
+            $this->assertSame('not_found', $response->getBody()['error']['code']);
+        }
+
+        $this->assertTrue($thrownException);
+    }
 }
